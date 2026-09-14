ecfa32ba40051fcaecfe478a7f52e31853a8f847
MuMing Deng <48084715+dengmuming@users.noreply.github.com>
2026-09-14 15:08:25 +0800

    feat(manifold): export shared loop views

---
+++ packages/views/loops/index.ts
@@ -0,0 +1,22 @@
+export { LoopList } from "./components/loop-list";
+export type { LoopListProps } from "./components/loop-list";
+export { LoopDetailPanel } from "./components/loop-detail-panel";
+export type { LoopDetailPanelProps } from "./components/loop-detail-panel";
+export {
+  currentLoopStage,
+  findingsForLoop,
+  groupLoopNodesByStage,
+  nodeIsSatisfied,
+  workflowRetryTotal,
+} from "./loop-utils";
+export type { LoopStageView } from "./loop-utils";
+export type {
+  LoopArtifactView,
+  LoopDetailView,
+  LoopFindingView,
+  LoopListCopy,
+  LoopNodeView,
+  LoopState,
+  LoopSummaryView,
+  LoopViewCopy,
+} from "./types";
