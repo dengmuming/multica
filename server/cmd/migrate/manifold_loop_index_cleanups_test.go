5acb4bf4d3bf5e460b5365ea42f97f2c46b0d800
MuMing Deng <48084715+dengmuming@users.noreply.github.com>
2026-09-14 11:07:31 +0800

    test(manifold): cover evaluation task uniqueness cleanup hook

--- server/cmd/migrate/manifold_loop_index_cleanups_test.go
+++ server/cmd/migrate/manifold_loop_index_cleanups_test.go
@@ -17,6 +17,7 @@ func TestManifoldLoopConcurrentIndexesHaveCleanupHooks(t *testing.T) {
 		"479_loop_artifact_node_idx":      "idx_loop_artifact_node_created",
 		"480_loop_artifact_task_idx":      "idx_loop_artifact_task",
 		"481_loop_instance_key_uq":        "idx_issue_manifold_loop_instance_key",
+		"483_loop_evaluation_task_uq":     "idx_loop_evaluation_one_per_task",
 	}
 
 	for version, index := range want {
