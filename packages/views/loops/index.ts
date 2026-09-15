904dd8a02b3f117415b2a78bca0150822668fd06
MuMing Deng <48084715+dengmuming@users.noreply.github.com>
2026-09-15 15:54:53 +0800

    feat(manifold): export Work Graph view

--- packages/views/loops/index.ts
+++ packages/views/loops/index.ts
@@ -4,22 +4,7 @@ export { LoopList } from "./components/loop-list";
 export type { LoopListProps } from "./components/loop-list";
 export { LoopDetailPanel } from "./components/loop-detail-panel";
 export type { LoopDetailPanelProps } from "./components/loop-detail-panel";
-export {
-  currentLoopStage,
-  findingsForLoop,
-  groupLoopNodesByStage,
-  nodeIsSatisfied,
-  requiredLoopProgress,
-  workflowRetryTotal,
-} from "./loop-utils";
+export { WorkGraph } from "./components/work-graph";
+export { currentLoopStage, findingsForLoop, groupLoopNodesByStage, nodeIsSatisfied, requiredLoopProgress, workflowRetryTotal } from "./loop-utils";
 export type { LoopStageState, LoopStageView } from "./loop-utils";
-export type {
-  LoopArtifactView,
-  LoopDetailView,
-  LoopFindingView,
-  LoopListCopy,
-  LoopNodeView,
-  LoopState,
-  LoopSummaryView,
-  LoopViewCopy,
-} from "./types";
+export type { LoopArtifactView, LoopDetailView, LoopFindingView, LoopListCopy, LoopNodeView, LoopState, LoopSummaryView, LoopViewCopy } from "./types";
